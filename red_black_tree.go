5a6148221dace22dfaa01ab0435cf136951d139e
agschipor <agschipor@gmail.com>
2018-01-20 01:46:46 +0200

    testing only

--- red_black_tree.go
+++ red_black_tree.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
-func main () {
-	fmt.Println("Hello, world!")	
+func asr(a, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println("Hello, world!")
+	fmt.Println(asr(3, 2))
 }

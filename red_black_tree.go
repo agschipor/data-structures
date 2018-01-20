a9446cd370310bf3b0a808098db66facfe475a78
agschipor <agschipor@gmail.com>
2018-01-20 02:32:22 +0200

    head node

--- red_black_tree.go
+++ red_black_tree.go
@@ -4,6 +4,25 @@ import (
 	"fmt"
 )
 
+type Node struct {
+	left   *Node
+	parent *Node
+	right  *Node
+	isnil  bool
+	value  int
+}
+
+func CreateHeadNode() *Node {
+	var head Node = Node{}
+	head.left = &head
+	head.right = &head
+	head.parent = &head
+	head.isnil = true
+	head.value = 0
+	return &head
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	var head *Node = CreateHeadNode()
+	fmt.Println(head.value, head, head.parent)
 }
